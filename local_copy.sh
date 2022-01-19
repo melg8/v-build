93b95eca1507c38200c41e2aa292f63d8de64028
v-str <w.strepetov@gmail.com>
2022-01-19 17:50:51 +0300

    Launch app from separate directory

--- local_copy.sh
+++ local_copy.sh
@@ -6,3 +6,9 @@ cp -v ../v-build_build/navigation_lib/libnavigation_lib.so.1.0.0 system/libs/lib
 rm -rfv ../v-build_build/v-build/system
 mkdir ../v-build_build/v-build/system
 cp -rv system/* ../v-build_build/v-build/system
+
+rm -rf ../v-build_run
+mkdir ../v-build_run
+mkdir ../v-build_run/system
+cp -v ../v-build_build/v-build/v-build ../v-build_run/v-build
+cp -rv ../v-build_build/v-build/system/* ../v-build_run/system
