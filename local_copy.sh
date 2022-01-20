1f9593bab153c145813d390db43a8dd8c5f71f44
v-str <w.strepetov@gmail.com>
2022-01-20 17:09:56 +0300

    Fix local_copy script

--- local_copy.sh
+++ local_copy.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-cp -v ../v-build_build/io_lib/libio_lib.so.1.0.0 system/libs/libio_lib.so.1.0.0
-cp -v ../v-build_build/navigation_lib/libnavigation_lib.so.1.0.0 system/libs/libnavigation_lib.so.1.0.0
-cp -v ../v-build_build/osx64_lib/libosx64_lib.so.1.0.0 system/libs/libosx64_lib.so.1.0.0
+cp ../v-build_build/io_lib/libio_lib.so.1.0.0 system/libs/libio_lib.so.1.0.0
+cp ../v-build_build/navigation_lib/libnavigation_lib.so.1.0.0 system/libs/libnavigation_lib.so.1.0.0
+cp ../v-build_build/osx64_lib/libosx64_lib.so.1.0.0 system/libs/libosx64_lib.so.1.0.0
 
-rm -rfv ../v-build_build/v-build/system
+rm -rf ../v-build_build/v-build/system
 mkdir ../v-build_build/v-build/system
 
-cp -rv system/* ../v-build_build/v-build/system
+cp -r system/* ../v-build_build/v-build/system
 
-rm -rf ../v-build_run
-mkdir ../v-build_run
-mkdir ../v-build_run/system
+if [ -d "../v-build_run" ]; then
+	rm -rf ../v-build_run/system
+	rm -rf ../v-build_run/v-build
+	mkdir ../v-build_run/system
+else
+	mkdir ../v-build_run
+	mkdir ../v-build_run/system
+fi
 
-cp -v ../v-build_build/v-build/v-build ../v-build_run/v-build
-cp -rv ../v-build_build/v-build/system/* ../v-build_run/system
+cp ../v-build_build/v-build/v-build ../v-build_run/v-build
+cp -r ../v-build_build/v-build/system/* ../v-build_run/system
+
