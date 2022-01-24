a662660e0562abfa39cb306a5c5e27197453ae2c
v-str <w.strepetov@gmail.com>
2022-01-24 19:10:04 +0300

    Add download script for all deps building

--- system/scripts/osx64/common/download.sh
+++ system/scripts/osx64/common/download.sh
@@ -6,11 +6,13 @@ PKG_LIST="$V_BUILD_SYSTEM/scripts/osx64/packages.list"
 PATCH_LIST="$V_BUILD_SYSTEM/scripts/osx64/patches.list"
 
 if [ ! -d "$V_BUILD_ARCHIVE_DIR" ]; then
-	mkdir $V_BUILD_ARCHIVE_DIR
+	printf "Directory for archives doesn't exist, check env variables\n"
+	exit 1
 fi
 
-if [ -d "$V_BUILD_PATCH_DIR" ]; then
-	mkdir $V_BUILD_PATCH_DIR
+if [ ! -d "$V_BUILD_PATCH_DIR" ]; then
+	printf "Directory for patches doesn't exist, check env variables\n"
+	exit 1
 fi
 
 wget --input-file=$PKG_LIST --continue --directory-prefix=$V_BUILD_ARCHIVE_DIR
