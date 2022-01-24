55518d5505b40153be0e8fbec6bf07f606a0011a
v-str <w.strepetov@gmail.com>
2022-01-24 21:02:51 +0300

    Build binutils pass-1

--- system/scripts/osx64/system/make_binutils_x86_64_pass_1.sh
+++ system/scripts/osx64/system/make_binutils_x86_64_pass_1.sh
@@ -17,9 +17,6 @@ function msg(){ printf "${NC}$1 $2${NC}\n" ; }
 function msg_green(){ printf "\n${NC}$1 ${GREEN}$2${NC}\n" ; }
 function msg_red(){ printf "\n${NC}$1 ${RED}$2${NC}\n" ; }
 
-# determind arch type
-ARCH=$V_BUILD_TGT_X86_64
-
 ################################## Functions ###################################
 
 # find package in PKG dir
@@ -49,7 +46,7 @@ function install_binutils_pass_1(){
 	sh $V_BUILD_PKG_DIR/$BINUTILS/$BINUTILS/configure \
 	--prefix=$V_BUILD_TOOLS_X86_64 \
 	--with-sysroot=$V_BUILD_TREE_X86_64 \
-	--target=$ARCH \
+	--target=$V_BUILD_TGT_X86_64 \
 	--disable-nls \
 	--disable-werror
 	
@@ -62,12 +59,10 @@ function install_binutils_pass_1(){
 ################################## Main ########################################
 
 if [ ! -f "${V_BUILD_DIR}/.extracted" ]; then
-	msg_red "Packages are" "not extracted!"
-	msg "Run: make extract\n"
+	msg_red "" "packages are not extracted yet"
 	exit 1
 elif [ ! -d "${V_BUILD_TREE_X86_64}" ]; then
-	msg_red "V BUILD TREE" "do not exist!"
-	msg "Run: make os-tree-x86_64\n"
+	msg_red "" "tree do not exist!"
 	exit 1
 else
 	BINUTILS=$(find_package "binutils")
