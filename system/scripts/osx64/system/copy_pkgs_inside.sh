e0bfb47a45fec51033288b9dcef10ca109cc9831
v-str <w.strepetov@gmail.com>
2022-01-27 20:41:05 +0300

    copy packages and kernel in tree

---
+++ system/scripts/osx64/system/copy_pkgs_inside.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+function msg(){ printf "${NC}$1 $2${NC}\n" ; }
+function msg_green(){ printf "\n${NC}$1 ${GREEN}$2${NC}\n\n" ; }
+function msg_red(){ printf "\n${NC}$1 ${RED}$2${NC}\n\n" ; }
+
+if [ ! -d "${V_BUILD_TREE_X86_64}" ]; then
+	msg_red "\$V_BUILD_TREE" "do not exist!"
+	exit 1
+fi
+
+if [ -d "${V_BUILD_PKG_DIR}/packages" ]; then
+	msg_red "directory \"packages\" inside tree" "already exist!"
+	exit 1
+else
+	cp -r "${V_BUILD_PKG_DIR}" "${V_BUILD_TREE_X86_64}/"
+fi
+
+if [ ! -d "${V_BUILD_KERNEL_X86_64}" ]; then
+	msg_red "\$V_BUILD_KERNEL_X86_64" "do not exist!"
+	exit 1
+else
+	cp -r "${V_BUILD_KERNEL_X86_64}" "${V_BUILD_TREE_X86_64}/"
+fi
+
+sync
+
+msg_green "status:" "done."
+
+exit 0
+
+
