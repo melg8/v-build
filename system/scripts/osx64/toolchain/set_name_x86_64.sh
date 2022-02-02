e14e5fb3599a140a98811a52dfe7685e989f7ae0
v-str <w.strepetov@gmail.com>
2022-02-02 16:02:21 +0300

    Rename one of osx64 directories, set concrete name

--- system/scripts/osx64/toolchain/set_name_x86_64.sh
+++ system/scripts/osx64/toolchain/set_name_x86_64.sh
@@ -2,6 +2,11 @@
 
 # set `uname -m` inside building directory in file ".machine_name"
 
+if [ -e ".machine_name" ]; then
+	printf "file \".machine_name\" already exist, check it\n"
+	exit 0
+fi
+
 uname -m >> .machine_name
 
 exit 0
