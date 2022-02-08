89e389274d7b35738001fbfb7f3ebc489b675943
v-str <w.strepetov@gmail.com>
2022-02-08 20:21:43 +0300

    Rename packages for clear invocation

---
+++ system/parts/basic_system/gcc_pkg.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+function msg(){ printf "${NC}$1 $2${NC}\n" ; }
+function msg_green(){ printf "\n${NC}$1 ${GREEN}$2${NC}\n\n" ; }
+function msg_red(){ printf "\n${NC}$1 ${RED}$2${NC}\n\n" ; }
+
+search_pkg=`sh /parts/basic_system/find_package.sh "gcc"`
+
+if [ ! -z "${search_pkg}" ]; then
+	msg_green "package found:" "$search_pkg"
+else
+	msg_res "package not found:" "$search_pkg"
+	exit 1
+fi
+
+pushd /packages/${search_pkg}/${search_pkg}
+
+# fix an issue breaking libasan.a building this package with Glibc-2.34
+sed -e '/static.*SIGSTKSZ/d' \
+    -e 's/return kAltStackSize/return SIGSTKSZ * 4/' \
+    -i libsanitizer/sanitizer_common/sanitizer_posix_libcdep.cpp
+
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+  ;;
+esac
+
+rm -rf build
+mkdir -v build
+pushd build
+
+../configure --prefix=/usr            \
+             LD=ld                    \
+             --enable-languages=c,c++ \
+             --disable-multilib       \
+             --disable-bootstrap      \
+             --with-system-zlib
+
+make
+
+# increase stack size for tests
+ulimit -s 32768
+
+make -k check
+
+make install
+rm -rf /usr/lib/gcc/$(gcc -dumpmachine)/*/include-fixed/bits/
+
+chown -v -R root:root \
+    /usr/lib/gcc/*linux-gnu/*/include{,-fixed}
+
+ln -svr /usr/bin/cpp /usr/lib
+
+ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/*/liblto_plugin.so \
+        /usr/lib/bfd-plugins/
+
+echo 'int main(){}' > dummy.c
+cc dummy.c -v -Wl,--verbose &> dummy.log
+readelf -l a.out | grep ': /lib'
+
+grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
+
+grep -B4 '^ /usr/include' dummy.log
+
+grep 'SEARCH.*/usr/lib' dummy.log |sed 's|; |\n|g'
+
+grep "/lib.*/libc.so.6 " dummy.log
+
+grep found dummy.log
+
+rm -v dummy.c a.out dummy.log
+
+mkdir -pv /usr/share/gdb/auto-load/usr/lib
+mv -v /usr/lib/*gdb.py /usr/share/gdb/auto-load/usr/lib
+
+
+popd
+
+popd
+
+exit 0
+
+
---
+++ system/parts/basic_system/shadow_pkg.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+function msg(){ printf "${NC}$1 $2${NC}\n" ; }
+function msg_green(){ printf "\n${NC}$1 ${GREEN}$2${NC}\n\n" ; }
+function msg_red(){ printf "\n${NC}$1 ${RED}$2${NC}\n\n" ; }
+
+search_pkg=`sh /parts/basic_system/find_package.sh "shadow"`
+
+if [ ! -z "${search_pkg}" ]; then
+	msg_green "package found:" "$search_pkg"
+else
+	msg_res "package not found:" "$search_pkg"
+	exit 1
+fi
+
+pushd /packages/${search_pkg}/${search_pkg}
+
+# Disable the installation of the groups program and its man pages
+sed -i 's/groups$(EXEEXT) //' src/Makefile.in
+find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
+find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
+find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
+
+sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD SHA512:' \
+    -e 's:/var/spool/mail:/var/mail:'                 \
+    -e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                \
+    -i etc/login.defs
+
+# fix programming errors
+sed -e "224s/rounds/min_rounds/" -i libmisc/salt.c
+
+touch /usr/bin/passwd
+./configure --sysconfdir=/etc \
+            --with-group-name-max-length=32
+
+make
+
+make exec_prefix=/usr install
+make -C man install-man
+mkdir -p /etc/default
+useradd -D --gid 999
+
+pwconv
+grpconv
+
+popd
+
+exit 0
+
+
